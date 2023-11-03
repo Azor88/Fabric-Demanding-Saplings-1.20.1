cf6749133c567b25fac8b33e78be40f970c4f677
azor88 <aozor.888@gmail.com>
2023-11-03 09:35:06 -0600

    Organized ModModelPredicateProvider code for easier reading.

--- src/main/java/net/azor/demandingsaplings/util/ModModelPredicateProvider.java
+++ src/main/java/net/azor/demandingsaplings/util/ModModelPredicateProvider.java
@@ -5,6 +5,7 @@ import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
 
 public class ModModelPredicateProvider {
     public static void registerModModels()
@@ -15,64 +16,68 @@ public class ModModelPredicateProvider {
     private static void registerThermometer()
     {
         ModelPredicateProviderRegistry.register(ModItems.THERMOMETER, new Identifier("temp_measurement"), (stack, world, livingEntity, seed) -> {
+
             Entity entity = livingEntity != null ? livingEntity : stack.getHolder();
             if (entity == null)
             {
                 return 0.0f;
             }
+
+            if (world == null && ((Entity)entity).getWorld() instanceof ClientWorld)
+            {
+                world = (ClientWorld)((Entity)entity).getWorld();
+            }
+
+            if (world == null || livingEntity == null)
+            {
+                return 0.0f;
+            }
             else
             {
-                if (world == null && ((Entity)entity).getWorld() instanceof ClientWorld)
-                {
-                    world = (ClientWorld)((Entity)entity).getWorld();
-                }
-
-                if (world == null || livingEntity == null)
-                {
-                    return 0.0f;
-                }
-                else
-                {
-                    float posY = entity.getBlockY();
-                    float temp = world.getBiomeAccess().getBiome(entity.getBlockPos()).value().getTemperature();
-
-
-                    double tempBioma = 0;
-                    double tempRestador = 0.00125;
-
-                    if (posY <= 80)
-                    {
-                        tempBioma = temp;
-                    }
-                    else if (posY >= 81)
-                    {
-                        int altura = (int)posY - 81;
-                        double totalResta = altura * tempRestador;
-                        tempBioma = temp - totalResta;
-                    }
-
-                    if (tempBioma < -0.4)
-                    {
-                        return 0;
-                    }
-                    else if (tempBioma < 0.2)
-                    {
-                        return 1;
-                    }
-                    else if (tempBioma < 0.8)
-                    {
-                        return 2;
-                    }
-                    else if (tempBioma < 1.4)
-                    {
-                        return 3;
-                    }
-                    else
-                    {
-                        return 4;
-                    }
-                }
+                return getTempValue(world, entity);
             }
         });
+    }
+
+    public static int getTempValue(World world, Entity entity)
+    {
+        float posY = entity.getBlockY();
+        float temp = world.getBiomeAccess().getBiome(entity.getBlockPos()).value().getTemperature();
+
+
+        double tempBioma = 0;
+        double tempRestador = 0.00125;
+
+        if (posY <= 80)
+        {
+            tempBioma = temp;
+        }
+        else if (posY >= 81)
+        {
+            int altura = (int)posY - 81;
+            double totalResta = altura * tempRestador;
+            tempBioma = temp - totalResta;
+        }
+
+        if (tempBioma < -0.4f)
+        {
+            return 0;
+        }
+        else if (tempBioma < 0.2f)
+        {
+            return 1;
+        }
+        else if (tempBioma < 0.8f)
+        {
+            return 2;
+        }
+        else if (tempBioma < 1.4f)
+        {
+            return 3;
+        }
+        else
+        {
+            return 4;
+        }
     }
 }
