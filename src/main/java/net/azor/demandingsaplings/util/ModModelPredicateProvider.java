97c9f6d1524a6273eb42d7e83d5b1be96d9417b3
azor88 <aozor.888@gmail.com>
2023-11-05 11:12:01 -0600

    Thermometer now displays textures correctly given the temperature

--- src/main/java/net/azor/demandingsaplings/util/ModModelPredicateProvider.java
+++ src/main/java/net/azor/demandingsaplings/util/ModModelPredicateProvider.java
@@ -13,71 +13,57 @@ public class ModModelPredicateProvider {
         registerThermometer();
     }
 
-    private static void registerThermometer()
-    {
+    private static void registerThermometer() {
         ModelPredicateProviderRegistry.register(ModItems.THERMOMETER, new Identifier("temp_measurement"), (stack, world, livingEntity, seed) -> {
 
             Entity entity = livingEntity != null ? livingEntity : stack.getHolder();
-            if (entity == null)
-            {
+            if (entity == null) {
                 return 0.0f;
             }
 
-            if (world == null && ((Entity)entity).getWorld() instanceof ClientWorld)
-            {
+            if (world == null && ((Entity)entity).getWorld() instanceof ClientWorld) {
                 world = (ClientWorld)((Entity)entity).getWorld();
             }
 
-            if (world == null || livingEntity == null)
-            {
+            if (world == null) {
                 return 0.0f;
             }
-            else
-            {
+            else {
                 return getTempValue(world, entity);
             }
         });
     }
 
-    public static int getTempValue(World world, Entity entity)
-    {
+    public static float getTempValue(World world, Entity entity) {
         float posY = entity.getBlockY();
         float temp = world.getBiomeAccess().getBiome(entity.getBlockPos()).value().getTemperature();
-
 
         double tempBioma = 0;
         double tempRestador = 0.00125;
 
-        if (posY <= 80)
-        {
+        if (posY <= 80) {
             tempBioma = temp;
         }
-        else if (posY >= 81)
-        {
+        else if (posY >= 81) {
             int altura = (int)posY - 81;
             double totalResta = altura * tempRestador;
             tempBioma = temp - totalResta;
         }
 
-        if (tempBioma < -0.4f)
-        {
-            return 0;
+        if (tempBioma < -0.4f) {
+            return 0.0f;
         }
-        else if (tempBioma < 0.2f)
-        {
-            return 1;
+        else if (tempBioma < 0.2f) {
+            return 0.2f;
         }
-        else if (tempBioma < 0.8f)
-        {
-            return 2;
+        else if (tempBioma < 0.8f) {
+            return 0.4f;
         }
-        else if (tempBioma < 1.4f)
-        {
-            return 3;
+        else if (tempBioma < 1.4f) {
+            return 0.6f;
         }
-        else
-        {
-            return 4;
+        else {
+            return 0.8f;
         }
     }
 }
